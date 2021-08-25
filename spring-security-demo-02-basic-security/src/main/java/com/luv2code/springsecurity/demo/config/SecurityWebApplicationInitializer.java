a7fd635926f1a1b986b97ae178f2c94623befe5a
Coditas <Coditas@DESKTOP-GEVL235>
2021-08-25 17:37:18 +0530

    New Security Project: Creating Security initializer, Security Config and Users (Users, Passwords and Roles)

---
+++ spring-security-demo-02-basic-security/src/main/java/com/luv2code/springsecurity/demo/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,9 @@
+package com.luv2code.springsecurity.demo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+
+	// There is no need to do anything here.
+	// We just need to Extend the class.
+}
