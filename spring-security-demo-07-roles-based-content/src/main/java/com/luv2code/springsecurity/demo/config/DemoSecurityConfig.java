dcb2a10bad774451452ad0eb6c6f781953a19c03
Coditas <Coditas@DESKTOP-GEVL235>
2021-08-26 15:12:12 +0530

    Spring Security: Restrict Access Based On Role

---
+++ spring-security-demo-07-roles-based-content/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,55 @@
+package com.luv2code.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+		// add our users for in memory authentication
+		
+		UserBuilder users = User.withDefaultPasswordEncoder();
+		
+		auth.inMemoryAuthentication()
+			.withUser(users.username("john").password("test123").roles("EMPLOYEE"))
+			.withUser(users.username("mary").password("test123").roles("EMPLOYEE", "MANAGER"))
+			.withUser(users.username("susan").password("test123").roles("EMPLOYEE", "ADMIN"));
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests()
+			.antMatchers("/").hasRole("EMPLOYEE") // For restricting roles. Employee will be able to access only home page.
+			.antMatchers("/leaders/**").hasRole("MANAGER") // /leader/** path will only be accessible by MANAGER roles.
+			.antMatchers("/systems/**").hasRole("ADMIN") // /systems/** path will only be accessible by ADMIN roles.
+			.and()
+			.formLogin()
+				.loginPage("/showMyLoginPage")
+				.loginProcessingUrl("/authenticateTheUser")
+				.permitAll()
+			.and()
+			.logout().permitAll()
+			.and()
+			.exceptionHandling().accessDeniedPage("/access-denied"); // This our request mapping path for request denied page.
+					// So it will show this(/access-denied) path and its corresponding view page instead of default access denied page
+					// when the user is trying to access the restricted page.
+		
+	}
+		
+}
+
+
+
+
+
+
